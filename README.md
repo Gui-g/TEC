56d75974059d923ce28bbc2a6ce3f5bcecfb7a18
Gui-g <guilira94@gmail.com>
2021-04-06 19:15:20 -0300

    readme, trab done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TEC
+How To:
+1. Colocar arquivos para serem convertidos em /src
+OBS: o arquivo precisa ter uma ultima linha em branco
+2. python3 TrabMT
--- TrabMT.py
+++ TrabMT.py
@@ -8,6 +8,7 @@ class TuringAux:
         self.type = tm_type
         self.states = states
         self.operations = operations
+        self.initial_state = "0"
 
 class CMD:
     def __init__(self, current_state, current_symbol, new_symbol, direction, new_state):
@@ -17,32 +18,32 @@ class CMD:
         self.direction = direction
         self.new_state = new_state
 
+    def string(self):
+        return str(self)
+
 #colocar símbolo especial # no começo da fita, para qualquer estado se ler # voltar
 def S_to_I(turing_machine):
 
     turing_machine.type = "I"
 
-    #aumenta o número do estado em 1 para criar um novo estado 0 que prepara a fita
+    #mudar o número do estado com 1 para criar um novo estado 0 que prepara a fita
     for op in turing_machine.operations:
-        if op.new_state == "halt-accept":
-            op.current_state = int(op.current_state) + 1
-        else:
-            op.current_state = int(op.current_state) + 1
-            op.new_state = int(op.new_state) + 1
+        op.current_state = op.current_state + "1"
+        op.new_state = op.new_state + "1"
 
     new_state = list()
     for state in turing_machine.states:
-        new_state.append(int(state) + 1)
+        new_state.append(state + "1")
 
     turing_machine.states = new_state
 
     #para todo estado uma regra que caso leia # voltar para a fita
     for state in turing_machine.states:
-        turing_machine.operations.append(CMD(state,"#","#","r",state))
+        turing_machine.operations.append(CMD(state,"§","§","r",state))
 
     #comando para colocar # no começo da fita
-    turing_machine.operations.insert(0,CMD(0,"*","*","l",100))
-    turing_machine.operations.insert(1,CMD(100,"_","#","r",1)) #voltar para o estado inicial original
+    turing_machine.operations.insert(0,CMD(0,"*","*","l","step"))
+    turing_machine.operations.insert(1,CMD("step","_","§","r",turing_machine.initial_state+"1")) #voltar para o estado inicial original
 
     return turing_machine
 
@@ -51,65 +52,62 @@ def I_to_S(turing_machine):
 
     turing_machine.type = "S"
 
-    #aumenta o número do estado em 1 para criar um novo estado 0 que prepara a fita
+    #mudar o número do estado com 1 para criar um novo estado 0 que prepara a fita
     for op in turing_machine.operations:
-        if op.new_state == "halt-accept":
-            op.current_state = int(op.current_state) + 1
-        else:
-            op.current_state = int(op.current_state) + 1
-            op.new_state = int(op.new_state) + 1
+        op.current_state = op.current_state + "1"
+        op.new_state = op.new_state + "1"
 
     new_state = list()
     for state in turing_machine.states:
-        new_state.append(int(state) + 1)
+        new_state.append(state + "1")
 
     turing_machine.states = new_state
 
     #primeiro espaço:
     #dependendo do que lê (1 ou 0) vai para estados intermediários para manter a informação salva enquanto move fita
-    turing_machine.operations.insert(0,CMD(0,"1","#","r",101)) #1
-    turing_machine.operations.insert(1,CMD(0,"0","#","r",102)) #0
-    turing_machine.operations.insert(2,CMD(101,"1","_","l",103)) #11
-    turing_machine.operations.insert(3,CMD(101,"0","_","l",104)) #10
-    turing_machine.operations.insert(4,CMD(102,"1","_","l",105)) #01
-    turing_machine.operations.insert(5,CMD(102,"0","_","l",106)) #00
-    turing_machine.operations.insert(6,CMD(103,"#","#","r",103)) #11
-    turing_machine.operations.insert(7,CMD(103,"_","_","r",103)) #11
-    turing_machine.operations.insert(8,CMD(103,"1","1","r",107)) #11
-    turing_machine.operations.insert(9,CMD(103,"0","1","r",108)) #10
-    turing_machine.operations.insert(10,CMD(104,"#","#","r",104)) #10
-    turing_machine.operations.insert(11,CMD(104,"_","_","r",104)) #10
-    turing_machine.operations.insert(12,CMD(104,"1","1","r",109)) #01
-    turing_machine.operations.insert(13,CMD(104,"0","1","r",110)) #00
-    turing_machine.operations.insert(14,CMD(105,"#","#","r",105)) #01
-    turing_machine.operations.insert(15,CMD(105,"_","_","r",105)) #01
-    turing_machine.operations.insert(16,CMD(105,"1","0","r",107)) #11
-    turing_machine.operations.insert(17,CMD(105,"0","0","r",108)) #10
-    turing_machine.operations.insert(18,CMD(106,"#","#","r",106)) #00
-    turing_machine.operations.insert(19,CMD(106,"_","_","r",106)) #00
-    turing_machine.operations.insert(20,CMD(106,"1","0","r",109)) #01
-    turing_machine.operations.insert(21,CMD(106,"0","0","r",110)) #00
-    turing_machine.operations.insert(22,CMD(107,"_","1","r",111)) #1
-    turing_machine.operations.insert(23,CMD(107,"1","1","r",107)) #11
-    turing_machine.operations.insert(24,CMD(107,"0","1","r",108)) #10
-    turing_machine.operations.insert(25,CMD(108,"_","1","r",112)) #0
-    turing_machine.operations.insert(26,CMD(108,"1","1","r",109)) #01
-    turing_machine.operations.insert(27,CMD(108,"0","1","r",110)) #00
-    turing_machine.operations.insert(28,CMD(109,"_","0","r",111)) #1
-    turing_machine.operations.insert(29,CMD(109,"1","0","r",107)) #11
-    turing_machine.operations.insert(30,CMD(109,"0","0","r",108)) #10
-    turing_machine.operations.insert(31,CMD(110,"_","0","r",112)) #0
-    turing_machine.operations.insert(32,CMD(110,"1","0","r",109)) #01
-    turing_machine.operations.insert(33,CMD(110,"0","0","r",110)) #00
-    turing_machine.operations.insert(34,CMD(111,"_","1","l",113)) #
-    turing_machine.operations.insert(35,CMD(112,"_","0","l",113)) #
-    turing_machine.operations.insert(36,CMD(113,"*","*","l",113)) #
-    turing_machine.operations.insert(37,CMD(113,"_","_","r",1)) #voltar para o estado inicial original  
+    turing_machine.operations.insert(0,CMD(0,"1","§","r","step1")) #1
+    turing_machine.operations.insert(1,CMD(0,"0","§","r","step2")) #0
+    turing_machine.operations.insert(2,CMD("step1","1","_","l","step3")) #11
+    turing_machine.operations.insert(3,CMD("step1","0","_","l","step4")) #10
+    turing_machine.operations.insert(4,CMD("step2","1","_","l","step5")) #01
+    turing_machine.operations.insert(5,CMD("step2","0","_","l","step6")) #00
+    turing_machine.operations.insert(6,CMD("step3","§","§","r","step3")) #11
+    turing_machine.operations.insert(7,CMD("step3","_","_","r","step3")) #11
+    turing_machine.operations.insert(8,CMD("step3","1","1","r","step7")) #11
+    turing_machine.operations.insert(9,CMD("step3","0","1","r","step8")) #10
+    turing_machine.operations.insert(10,CMD("step4","§","§","r","step4")) #10
+    turing_machine.operations.insert(11,CMD("step4","_","_","r","step4")) #10
+    turing_machine.operations.insert(12,CMD("step4","1","1","r","step9")) #01
+    turing_machine.operations.insert(13,CMD("step4","0","1","r","step10")) #00
+    turing_machine.operations.insert(14,CMD("step5","§","§","r","step5")) #01
+    turing_machine.operations.insert(15,CMD("step5","_","_","r","step5")) #01
+    turing_machine.operations.insert(16,CMD("step5","1","0","r","step7")) #11
+    turing_machine.operations.insert(17,CMD("step5","0","0","r","step8")) #10
+    turing_machine.operations.insert(18,CMD("step6","§","§","r","step6")) #00
+    turing_machine.operations.insert(19,CMD("step6","_","_","r","step6")) #00
+    turing_machine.operations.insert(20,CMD("step6","1","0","r","step9")) #01
+    turing_machine.operations.insert(21,CMD("step6","0","0","r","step10")) #00
+    turing_machine.operations.insert(22,CMD("step7","_","1","r","step11")) #1
+    turing_machine.operations.insert(23,CMD("step7","1","1","r","step7")) #11
+    turing_machine.operations.insert(24,CMD("step7","0","1","r","step8")) #10
+    turing_machine.operations.insert(25,CMD("step8","_","1","r","step12")) #0
+    turing_machine.operations.insert(26,CMD("step8","1","1","r","step9")) #01
+    turing_machine.operations.insert(27,CMD("step8","0","1","r","step10")) #00
+    turing_machine.operations.insert(28,CMD("step9","_","0","r","step11")) #1
+    turing_machine.operations.insert(29,CMD("step9","1","0","r","step7")) #11
+    turing_machine.operations.insert(30,CMD("step9","0","0","r","step8")) #10
+    turing_machine.operations.insert(31,CMD("step10","_","0","r","step12")) #0
+    turing_machine.operations.insert(32,CMD("step10","1","0","r","step9")) #01
+    turing_machine.operations.insert(33,CMD("step10","0","0","r","step10")) #00
+    turing_machine.operations.insert(34,CMD("step11","_","1","l","step13")) #
+    turing_machine.operations.insert(35,CMD("step12","_","0","l","step13")) #
+    turing_machine.operations.insert(36,CMD("step13","*","*","l","step13")) #
+    turing_machine.operations.insert(37,CMD("step13","_","_","r",turing_machine.initial_state+"1")) #voltar para o estado inicial original  
 
     #subrotina de espaço para cada estado
     #para qualquer estado, caso leia # então mover, colocar _ e empurrar fita para a direita
     for state in turing_machine.states:
-        turing_machine.operations.append(CMD(state,"#","#","r",str(state)+"subr"))
+        turing_machine.operations.append(CMD(state,"§","§","r",str(state)+"subr"))
         turing_machine.operations.append(CMD(str(state)+"subr","_","_","r",str(state)+"subr"))
         turing_machine.operations.append(CMD(str(state)+"subr","1","_","r",str(state)+"subr1")) #1
         turing_machine.operations.append(CMD(str(state)+"subr","0","_","r",str(state)+"subr2")) #0
@@ -120,7 +118,7 @@ def I_to_S(turing_machine):
         turing_machine.operations.append(CMD(str(state)+"subr2","0","0","r",str(state)+"subr2")) #0
         turing_machine.operations.append(CMD(str(state)+"subr2","_","0","r",str(state)+"subr3")) #
         turing_machine.operations.append(CMD(str(state)+"subr3","*","*","l",str(state)+"subr3")) #
-        turing_machine.operations.append(CMD(str(state)+"subr3","#","#","r",state)) #
+        turing_machine.operations.append(CMD(str(state)+"subr3","§","§","r",state)) #
     
     return turing_machine
 
